af1bf42f2fb5a9f3032e12e63b304cb3b4897073
webchills <office@webchills.com>
2019-08-10 12:47:31 +0200

    Fix für Uralt Bug in der Track Orders Sidebox

--- NEUE DATEIEN/includes/modules/sideboxes/DEINTEMPLATE/track_orders.php
+++ NEUE DATEIEN/includes/modules/sideboxes/DEINTEMPLATE/track_orders.php
@@ -4,45 +4,45 @@
  * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: track_orders.php 2019-08-06 20:19:14 webchills $
+ * @version $Id: track_orders.php 2019-08-10 12:12:14 webchills $
 */
 // test if track orders sidebox should show
   $show_track_orders= false;
 
 if ($show_track_orders = true) {
-  if (!zen_is_logged_in()) {
+  if (zen_is_logged_in()) {
 // retrieve the last x products purchased
-  $orders_history_query = 'select o.orders_id, o.date_purchased
+  $orders_history_tracking_query = 'select o.orders_id, o.date_purchased
                    from ' . TABLE_ORDERS . " o
                    where o.customers_id = '" . (int)$_SESSION['customer_id'] . "'
                    order by o.date_purchased desc
                    limit " . MAX_DISPLAY_PRODUCTS_IN_TRACK_ORDERS_BOX;
 
-    $orders_history = $db->Execute($orders_history_query);
+    $orders_history_tracking = $db->Execute($orders_history_tracking_query);
 
-    if ($orders_history->RecordCount() > 0) {
-      $orders_ids = '';
-      while (!$orders_history->EOF) {
-        $orders_ids .= (int)$orders_history->fields['orders_id'] . ',';
-        $orders_history->MoveNext();
+    if ($orders_history_tracking->RecordCount() > 0) {
+      $orders_history_ids = '';
+      while (!$orders_history_tracking->EOF) {
+        $orders_history_ids .= (int)$orders_history_tracking->fields['orders_id'] . ',';
+        $orders_history_tracking->MoveNext();
       }
-      $orders_ids = substr($orders_ids, 0, -1);
+      $orders_history_ids = substr($orders_history_ids, 0, -1);
       $rows=0;
-      $customer_orders_string = '<table border="0" width="100%" cellspacing="0" cellpadding="1">';
-      $products_history_query = 'select orders_id, date_purchased
+      $customer_orders_tracking_string = '<table border="0" width="100%" cellspacing="0" cellpadding="1">';
+      $products_history_tracking_query = 'select orders_id, date_purchased
                          from ' . TABLE_ORDERS . '
-                         where orders_id in (' . $orders_ids . ')
+                         where orders_id in (' . $orders_history_ids . ')
                          order by date_purchased desc';
 
-      $products_history = $db->Execute($products_history_query);
+      $products_history_tracking = $db->Execute($products_history_tracking_query);
 
-      while (!$products_history->EOF) {
+      while (!$products_history_tracking->EOF) {
         $rows++;
-        $customer_orders[$rows]['id'] = $products_history->fields['orders_id'];
-        $customer_orders[$rows]['date'] = $products_history->fields['date_purchased'];
-        $products_history->MoveNext();
+        $customer_orders_tracking[$rows]['id'] = $products_history_tracking->fields['orders_id'];
+        $customer_orders_tracking[$rows]['date'] = $products_history_tracking->fields['date_purchased'];
+        $products_history_tracking->MoveNext();
       }
-      $customer_orders_string .= '</table>';
+      $customer_orders_tracking_string .= '</table>';
     }
   }
       require $template->get_template_dir('tpl_track_orders.php',DIR_WS_TEMPLATE, $current_page_base,'sideboxes'). '/tpl_track_orders.php';
--- NEUE DATEIEN/includes/templates/DEINTEMPLATE/sideboxes/tpl_track_orders.php
+++ NEUE DATEIEN/includes/templates/DEINTEMPLATE/sideboxes/tpl_track_orders.php
@@ -4,22 +4,22 @@
  * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: tpl_track_orders.php 2019-08-06 20:05:14 webchills $
+ * @version $Id: tpl_track_orders.php 2019-08-10 12:05:14 webchills $
 */
   $content = "";
   $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent">' . "\n";
   
   
-  if (count($customer_orders)==0) {
+  if (sizeof($customer_orders_tracking)==0) {
      $content .= '<a href="' . zen_href_link(FILENAME_ACCOUNT_HISTORY_INFO, '','SSL') . '">'.PT_SIDEBOX_TRACKYOURORDER.'</a>';
  }
 else {
 	  $content .= '<ul class="orderHistList">' . "\n" ;
-  for ($i=1, $iMax = count($customer_orders); $i<= $iMax; $i++) {
+	  for ($i=1; $i<=sizeof($customer_orders_tracking); $i++) {
+ 
 
-        $content .= '<li><a href="' . zen_href_link(FILENAME_ACCOUNT_HISTORY_INFO, 'order_id=' . $customer_orders[$i]['id'],'SSL') . '">' .PT_SIDEBOX_ORDERNUMBER.'' . $customer_orders[$i]['id'] . '</a></li>' . "\n" ;
+        $content .= '<li><a href="' . zen_href_link(FILENAME_ACCOUNT_HISTORY_INFO, 'order_id=' . $customer_orders_tracking[$i]['id'],'SSL') . '">' .PT_SIDEBOX_ORDERNUMBER.'' . $customer_orders_tracking[$i]['id'] . '</a></li>' . "\n" ;
   }
   $content .= '</ul>' . "\n" ;
 }
   $content .= '</div>';
-?>
